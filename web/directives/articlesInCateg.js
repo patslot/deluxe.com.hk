810042894ff95623e0ed7be91e1b2ca8f1c04b83
WangSteve <stevewang@clustertech.com>
2017-03-28 17:26:24 +0800

    Style update 4 (#180)
    1. Add favicon
    2. Correct article list mobile order
    3. Modify upcoming events box on mobile to show 1 item height
    
    ref #95

--- web/directives/articlesInCateg.js
+++ web/directives/articlesInCateg.js
@@ -56,6 +56,16 @@ const htmlTpl = `
 <% articles.forEach(function(a) { %>
 <div class="al_two_col_container visible-xs">
   <div class="row">
+    <div class="al_two_image col-sm-5 col-md-6 col-xs-12">
+      <a href="<%= a.linkURL %>">
+        <div class="pos-relative">
+          <% if (a.hasVideo) { %>
+          <img class="play" src="/img/icon-play.png" />
+          <% } %>
+          <img src="<%= a.image %>" />
+        </div>
+      </a>
+    </div>
     <div class="al_two_content col-sm-5 col-md-6 col-xs-12">
       <div class="al_two_content_label">[<%= a.catName %>]</div>
       <a href="<%= a.linkURL %>">
@@ -64,16 +74,6 @@ const htmlTpl = `
         </div>
         <div class="other_article al_two_content_content">
           <%- a.content %>
-        </div>
-      </a>
-    </div>
-    <div class="al_two_image col-sm-5 col-md-6 col-xs-12">
-      <a href="<%= a.linkURL %>">
-        <div class="pos-relative">
-          <% if (a.hasVideo) { %>
-          <img class="play" src="/img/icon-play.png" />
-          <% } %>
-          <img src="<%= a.image %>" />
         </div>
       </a>
     </div>
