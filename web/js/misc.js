468aeadbdae61d71109bd01e2c07d762635a6597
WangSteve <stevewang@clustertech.com>
2017-03-31 13:24:53 +0800

    Fix mobile top menu jumping problem on scrolling (#223)

--- web/js/misc.js
+++ web/js/misc.js
@@ -1,30 +1,34 @@
 $(document).ready(function() {
   var minimizeHeader = function () {
-    var nmHeader = $('.nm_header');
-    var logo = $('.nm_header .index-logo .logo');
-    var stickyWrap = $('.nm_header #sticky-wrap');
-    var scrollTop = $(document).scrollTop();
-    var height = logo.height();
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      var nmHeader = $('.nm_header');
+      var logo = $('.nm_header .index-logo .logo');
+      var stickyWrap = $('.nm_header #sticky-wrap');
+      var scrollTop = $(document).scrollTop();
+      var height = logo.height();
 
-    if (scrollTop > height) {
-      nmHeader.addClass('fixed');
-    } else {
-      nmHeader.removeClass('fixed');
+      if (scrollTop > height) {
+        nmHeader.addClass('fixed');
+      } else {
+        nmHeader.removeClass('fixed');
+      }
     }
   }
 
   var updateSkinnerADPos = function () {
-    var skinnerAD = $('.skinner_ad_wrapper');
-    var header = $('.nm_header');
-    var logo = $('.nm_header .index-logo .logo');
-    var stickyWrap = $('.nm_header #sticky-wrap');
-    var scrollTop = $(document).scrollTop();
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      var skinnerAD = $('.skinner_ad_wrapper');
+      var header = $('.nm_header');
+      var logo = $('.nm_header .index-logo .logo');
+      var stickyWrap = $('.nm_header #sticky-wrap');
+      var scrollTop = $(document).scrollTop();
 
-    if (header.hasClass('fixed')) {
-      skinnerAD.css('top', stickyWrap.height());
-    } else {
-      var targetTop = logo.outerHeight(true) + stickyWrap.height() - scrollTop;
-      skinnerAD.css('top', targetTop);
+      if (header.hasClass('fixed')) {
+        skinnerAD.css('top', stickyWrap.height());
+      } else {
+        var targetTop = logo.outerHeight(true) + stickyWrap.height() - scrollTop;
+        skinnerAD.css('top', targetTop);
+      }
     }
   }
 
