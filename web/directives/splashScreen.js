815fd69c1e9c0648f8d9bb60684b6d14cb82f313
Nancy HO <ltho@clustertech.com>
2017-04-08 17:14:44 +0800

    only show splash screen if splash Ad available

--- web/directives/splashScreen.js
+++ web/directives/splashScreen.js
@@ -1,31 +1,53 @@
 import css from "../assets/css/splash.css";
 
 const htmlTpl = `
-<div class="<%= css.splash_section %> visible-xs">
-  <div class="<%= css.overlay %>">
-    <div class="<%= css.overlay_content %>">
-      <div class="<%= css.splash_wrapper %>">
-        <div id="splashad" class="<%= css.splash_content %>"></div>
-        <div class="<%= css.splash_close %>" id="adCloseBtn"></div>
-      </div>
-    </div>
-  </div>
+<div id="splashad" class="<%= css.splash_section %> visible-xs">
 </div>
 `;
 
-export default function($timeout) {
+const closeBtnHtmlTpl = `
+<div class="<%= css.splash_close_wrapper %>">
+  <div class="<%= css.splash_close %>" id="adCloseBtn"></div>
+</div>
+`;
+
+export default function() {
   return {
     restrict: 'E',
     link: function (scope, element) {
       scope.css = css;
       element.hide().html(ejs.render(htmlTpl, scope));
-      angular.element('#adCloseBtn').click(function() {
-        element.hide();
-      });
       // show splash ad if not come from next media or apple daily
       if (!/(http|https)?:\/\/[\w\.]{2,}.(nextmedia.com|appledaily.com)/i.test(document.referrer)) {
         element.show();
-        showMobileAd("SplashAd", "splashad");
+        showMobileAd("SplashAd", "splashad", function(isEmpty) {
+          if (isEmpty) {
+            element.hide();
+          } else {
+            angular.element("#splashad").addClass(css.splash_section_visible);
+            angular.element("#splashad").prepend(ejs.render(closeBtnHtmlTpl, scope));
+            angular.element('#adCloseBtn').click(function() {
+              element.hide();
+            });
+            try {
+              angular.element('#adCloseBtn').addEventListener("touchend", function(e){
+                e.stopPropagation();
+                e.preventDefault();
+                element.hide();
+                return false;
+              });
+            } catch(e) {
+            }
+            /* avoid windows phone 7.5 click through */
+            var isWindowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
+            if (isWindowsPhone) {
+              angular.element('#splashad').click(function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+              });
+            }
+          }
+        });
       }
     }
   };
