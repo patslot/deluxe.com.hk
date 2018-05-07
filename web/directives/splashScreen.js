7df47a5652c0a6214734cac1b59c171bb3d49706
Pat Kwok <patkwok@nextdigital.com.hk>
2018-05-07 17:52:31 +0800

    bug fix

--- web/directives/splashScreen.js
+++ web/directives/splashScreen.js
@@ -14,41 +14,50 @@ const closeBtnHtmlTpl = `
 export default function() {
   return {
     restrict: 'E',
-    link: function (scope, element) {
-      scope.css = css;
-      element.hide().html(ejs.render(htmlTpl, scope));
-      // show splash ad if not come from next media or apple daily
-      if (!/(http|https)?:\/\/[\w\.]{2,}.(nextmedia.com|appledaily.com)/i.test(document.referrer)) {
-        element.show();
-        showMobileAd("SplashAd", "splashad", function(isEmpty) {
-          if (isEmpty) {
-            element.hide();
-          } else {
-            angular.element("#splashad").addClass(css.splash_section_visible);
-            angular.element("#splashad").prepend(ejs.render(closeBtnHtmlTpl, scope));
-            angular.element('#adCloseBtn').click(function() {
-              element.hide();
-            });
-            try {
-              angular.element('#adCloseBtn').addEventListener("touchend", function(e){
-                e.stopPropagation();
-                e.preventDefault();
-                element.hide();
-                return false;
-              });
-            } catch(e) {
-            }
-            /* avoid windows phone 7.5 click through */
-            var isWindowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
-            if (isWindowsPhone) {
-              angular.element('#splashad').click(function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-              });
-            }
+    scope: {
+      articleId: '@articleId'
+    },
+    link: function (scope, element, attrs) {
+
+      var unwatch = scope.$watch(function(newVal) {
+          var articleId = newVal.articleId || "";
+          scope.css = css;
+          element.hide().html(ejs.render(htmlTpl, scope));
+          // show splash ad if not come from next media or apple daily
+          if (!/(http|https)?:\/\/[\w\.]{2,}.(nextmedia.com|appledaily.com)/i.test(document.referrer)) {
+            element.show();
+                showMobileAd("SplashAd", "splashad", articleId, function(isEmpty) {
+                  if (isEmpty) {
+                    element.hide();
+                  } else {
+                    angular.element("#splashad").addClass(css.splash_section_visible);
+                    angular.element("#splashad").prepend(ejs.render(closeBtnHtmlTpl, scope));
+                    angular.element('#adCloseBtn').click(function() {
+                      element.hide();
+                    });
+                    try {
+                      angular.element('#adCloseBtn').addEventListener("touchend", function(e){
+                        e.stopPropagation();
+                        e.preventDefault();
+                        element.hide();
+                        return false;
+                      });
+                    } catch(e) {
+                    }
+                    /* avoid windows phone 7.5 click through */
+                    var isWindowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
+                    if (isWindowsPhone) {
+                      angular.element('#splashad').click(function(e) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                      });
+                    }
+                  }
+                  
+                });
+            unwatch();
           }
-        });
-      }
+      });
     }
   };
 };
