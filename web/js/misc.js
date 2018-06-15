d976cfdb565269bcd5a5725d9c26e8058c5a7e3d
Pat Kwok <patkwok@nextdigital.com.hk>
2018-06-15 14:53:59 +0800

    GDPR fix

--- web/js/misc.js
+++ web/js/misc.js
@@ -1,28 +1,76 @@
 $(document).ready(function() {
   var updateSticky = function () {
-    if (window.matchMedia('(min-width: 768px)').matches) {
-			var nmHeader = $('.nm_header');
-      var stickyPlaceholder = $('.sticky-placeholder');
-      var skinnerAD = $('.skinner_ad_wrapper .skinner_ad');
-      var scrollTop = $(document).scrollTop();
-      var skinnerTrigerTop = $('.ls_banner').outerHeight(true) ? 160 + $('.ls_banner').outerHeight(true) : 160;
-      skinnerTrigerTop = $('.sub-categories').outerHeight(true) ? skinnerTrigerTop + $('.sub-categories').outerHeight(true) : skinnerTrigerTop;
-
+    var nmHeader = $('.nm_header');
+    var stickyPlaceholder = $('.sticky-placeholder');
+    var skinnerAD = $('.skinner_ad_wrapper .skinner_ad');
+    var scrollTop = $(document).scrollTop();
+    var GDPRLargeScreen = window.matchMedia('(min-width: 1025px)').matches;
+    var skinnerTrigerTop = $('.ls_banner').outerHeight(true) ? 160 + $('.ls_banner').outerHeight(true) : 160;
+    skinnerTrigerTop = $('.sub-categories').outerHeight(true) ? skinnerTrigerTop + $('.sub-categories').outerHeight(true) : skinnerTrigerTop;
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    if ( $('#div-GDPR-message').css('display') != "none" && $('#div-GDPR-message').length > 0 ) {
+      $('.nm_header').css('margin-top','65px');
+      var GDPR_height = $('#div-GDPR-message').outerHeight(true);
       if (scrollTop > 80) {
         nmHeader.addClass('fixed');
         stickyPlaceholder.addClass('fixed');
+        $('.nm_header.fixed #sticky-wrap .nav').css('top','65px');
       } else {
         nmHeader.removeClass('fixed');
         stickyPlaceholder.removeClass('fixed');
+        $('.nm_header #sticky-wrap .nav').css('top','auto');
       }
-
+      if (scrollTop > skinnerTrigerTop - 80) {
+        skinnerAD.css('top', 145);
+      } else {
+        skinnerAD.css('top', skinnerTrigerTop - scrollTop + 60);
+      }
+    }
+    else{
+      if (scrollTop > 80) {
+        nmHeader.addClass('fixed');
+        stickyPlaceholder.addClass('fixed');
+        $('.nm_header.fixed #sticky-wrap .nav').css('top','0px');
+      } else {
+        nmHeader.removeClass('fixed');
+        stickyPlaceholder.removeClass('fixed');
+        $('.nm_header #sticky-wrap .nav').css('top','auto');
+      }
       if (scrollTop > skinnerTrigerTop - 80) {
         skinnerAD.css('top', 80);
       } else {
         skinnerAD.css('top', skinnerTrigerTop - scrollTop);
       }
     }
-  };
+  }
+  else{
+    if ( $('#div-GDPR-message').css('display') != "none" && $('#div-GDPR-message').length > 0 ) {
+      $('.nm_header').css('margin-top','140px');
+      if (scrollTop > 80) {
+        nmHeader.addClass('fixed');
+        stickyPlaceholder.addClass('fixed');
+        $('.nm_header.fixed #sticky-wrap .nav').css('top','140px');
+      } else {
+        nmHeader.removeClass('fixed');
+        stickyPlaceholder.removeClass('fixed');
+        $('.nm_header #sticky-wrap .nav').css('top','auto');
+      }
+      if (scrollTop > skinnerTrigerTop - 80) {
+        skinnerAD.css('top', 145);
+      } else {
+        skinnerAD.css('top', skinnerTrigerTop - scrollTop + 60);
+      }
+    }
+  }
+};
+var GDPRcheck = setInterval(function(){
+      updateSticky();
+}, 100);
+
+setTimeout(function(){
+  window.clearInterval(GDPRcheck);
+},5000);
+
 
 	updateSticky();
   $(document).scroll(updateSticky);
