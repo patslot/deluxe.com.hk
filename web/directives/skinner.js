996daead463eef1db822534d91f30c17efc1e824
Pat Kwok <patkwok@nextdigital.com.hk>
2018-06-20 17:27:56 +0800

    AD Targeting bug fix

--- web/directives/skinner.js
+++ web/directives/skinner.js
@@ -5,12 +5,12 @@ const TEMPLATE = `
     <div class="skinner_ad_anchor" skinner_ad="align_top"></div>
     <div id="skycraper1" class="skinner_ad" skinner_ad_left>
       <script>
-        showWebAd("Skyscraper1", "skycraper1");
+        showWebAd("Skyscraper1", "skycraper1","<%= articleId %>");
       </script>
     </div>
     <div id="skycraper2" class="skinner_ad" skinner_ad_right>
       <script>
-        showWebAd("Skyscraper2", "skycraper2");
+        showWebAd("Skyscraper2", "skycraper2","<%= articleId %>");
       </script>
     </div>
   </div>
@@ -20,6 +20,16 @@ const TEMPLATE = `
 export default function() {
   return {
     restrict: 'E',
-    template: TEMPLATE
+    scope: {
+      articleId: '@articleId'
+    },
+    link: function (scope, element) {
+      var unwatch = scope.$watch(function(newVal) {
+        var articleId = newVal.articleId || "";
+        
+        element.html(ejs.render(TEMPLATE, {articleId: articleId}));
+        unwatch();
+      });
+    }
   };
 };
